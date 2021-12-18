ea5ecfe65ed536f77226b286ad51826a0e80130e
Sarah Gebauer <xgebi@users.noreply.github.com>
2021-12-18 21:33:14 +0100

    Login endpoint, buildable but untested 🤨

--- tusee-be/src/user/user.rs
+++ tusee-be/src/user/user.rs
@@ -1,6 +1,9 @@
 use std::error::Error;
 use actix_web::{get, post, web::{self, Data, ServiceConfig}, HttpRequest, HttpResponse, Responder};
-use serde::Serialize;
+use postgres::Row;
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+use bcrypt::{BcryptResult, DEFAULT_COST, hash, verify};
 use crate::utilities::utilities::create_database_connection;
 
 #[derive(Debug, Serialize)]
@@ -8,13 +11,13 @@ struct IsRegistrationEnabled {
     is_enabled: bool,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct UserDetails {
     username: String,
     password: String
 }
 
-#[derive(Debug, serialize)]
+#[derive(Debug, Serialize)]
 struct LoginResult {
     token: String,
     error: bool,
@@ -32,12 +35,62 @@ pub(crate) async fn is_registration_enabled() -> HttpResponse {
 
 #[post("/api/login")]
 pub(crate) async fn log_user_in(req_body: String) -> HttpResponse {
-    let conn = create_database_connection();
-    // conn.query_one("SELECT password FROM ")
+    let user_details: UserDetails;
+    match serde_json::from_str(req_body.as_str()) {
+        Ok(ud) => {
+            user_details = ud;
+        }
+        Err(_) => {
+            return HttpResponse::Ok()
+                .json(LoginResult {
+                    token: "".to_string(),
+                    error: true,
+                    error_message: "Could not login".to_string()
+                });
+        }
+    }
+    let mut conn = create_database_connection();
+    let row = conn.query_one("SELECT password FROM tusee_users WHERE email={}", &[&user_details.username]);
+    let mut login_result: LoginResult;
+    match row {
+        Ok(r) => {
+            let password_hash: String = r.get(0);
+            let valid = verify(user_details.password.as_str(), &password_hash.as_str());
+            match valid {
+                Ok(valid_result) => {
+                    if valid_result {
+                        login_result = LoginResult {
+                            token: "temporary token".to_string(),
+                            error: false,
+                            error_message: "".to_string()
+                        }
+                    } else {
+                        login_result = LoginResult {
+                            token: "".to_string(),
+                            error: true,
+                            error_message: "Could not login".to_string()
+                        };
+                    }
+                }
+                Err(_) => {
+                    login_result = LoginResult {
+                        token: "".to_string(),
+                        error: true,
+                        error_message: "Could not login".to_string(),
+                    };
+                }
+            }
+        }
+        Err(_) => {
+            login_result = LoginResult {
+                token: "".to_string(),
+                error: true,
+                error_message: "Could not login".to_string()
+            };
+        }
+    }
 
     let res = HttpResponse::Ok()
-        .json(IsRegistrationEnabled {
-            is_enabled: true,
-        });
+        .json(login_result);
     res
 }
--- tusee-be/src/utilities/utilities.rs
+++ tusee-be/src/utilities/utilities.rs
@@ -1,4 +1,4 @@
-use postgres::{Client, NoTls};
+use postgres::{Client, Error, NoTls};
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
@@ -16,8 +16,11 @@ pub(crate) fn create_database_connection() -> Client {
             let connection_string = format!("host={} port={} user={} password='{}' dbname={}",
                                             config.db_host, config.db_port, config.db_user,
                                             config.db_password, config.db_name);
-            let mut client = Client::connect(connection_string.as_str(), NoTls)?;
-            return client;
+            let mut client = Client::connect(connection_string.as_str(), NoTls);
+            match client {
+                Ok(c) => { return c; }
+                Err(error) => panic!("{:#?}", error)
+            }
         },
         Err(error) => panic!("{:#?}", error)
     }
